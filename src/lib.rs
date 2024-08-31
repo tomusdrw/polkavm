1a1972737dc83ecdc90542df3a845bcc17ecf2f0
Tomasz Drwięga <tomusdrw@gmail.com>
2024-08-31 22:07:11 +0200

    Update the API and make it less panicky.

--- src/lib.rs
+++ src/lib.rs
@@ -19,12 +19,12 @@ static PVM: Mutex<Option<Pvm>> = Mutex::new(None);
 pub struct Pvm {
     pc: u32,
     program: Vec<u8>,
-    gas: u32,
+    gas: i64,
     status: Status,
     registers: Vec<u8>,
 }
 impl Pvm {
-    fn new(program: Vec<u8>, registers: Vec<u8>, gas: u32) -> Self {
+    fn new(program: Vec<u8>, registers: Vec<u8>, gas: i64) -> Self {
         Self {
             pc: 0,
             program,
@@ -37,7 +37,7 @@ impl Pvm {
     fn next_step(&mut self) -> bool {
         self.pc += 1;
         if (self.pc as usize) < self.program.len() {
-            true 
+            true
         } else {
             self.status = Status::Halt;
             false
@@ -45,39 +45,49 @@ impl Pvm {
     }
 }
 
-fn with_pvm<F, R>(mut f: F) -> R where F: FnMut(&mut Pvm) -> R {
-    let mut pvm_l = PVM.lock().unwrap();
-    f(pvm_l.as_mut().unwrap())
+fn with_pvm<F, R>(mut f: F, default: R) -> R
+where
+    F: FnMut(&mut Pvm) -> R,
+{
+    let pvm_l = PVM.lock();
+    match pvm_l.ok() {
+        Some(mut guard) => match guard.as_mut() {
+            Some(pvm_l) => f(pvm_l),
+            None => default,
+        },
+        None => default,
+    }
 }
 
 #[wasm_bindgen]
-pub fn reset(program: Vec<u8>, registers: Vec<u8>, gas: u32) {
+pub fn reset(program: Vec<u8>, registers: Vec<u8>, gas: i64) {
     *PVM.lock().unwrap() = Some(Pvm::new(program, registers, gas));
 }
 
 #[wasm_bindgen]
 pub fn nextStep() -> bool {
-    with_pvm(|pvm| pvm.next_step())
+    with_pvm(|pvm| pvm.next_step(), false)
 }
 
 #[wasm_bindgen]
 pub fn getProgramCounter() -> u32 {
-    with_pvm(|pvm| pvm.pc)
+    with_pvm(|pvm| pvm.pc, 0)
 }
 
 #[wasm_bindgen]
 pub fn getStatus() -> Status {
-    with_pvm(|pvm| pvm.status)
+    with_pvm(|pvm| pvm.status, Status::Ok)
 }
 
 #[wasm_bindgen]
-pub fn getGasLeft() -> u32 {
-    with_pvm(|pvm| pvm.gas)
+pub fn getGasLeft() -> i64 {
+    with_pvm(|pvm| pvm.gas, 0)
 }
 
 #[wasm_bindgen]
 pub fn getRegisters() -> Vec<u8> {
-    with_pvm(|pvm| pvm.registers.clone())
+    let default_registers = vec![0; 13 * 4];
+    with_pvm(|pvm| pvm.registers.clone(), default_registers)
 }
 
 #[wasm_bindgen]
